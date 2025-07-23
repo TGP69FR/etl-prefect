d3bcbd3bcc0c60c54507b026bc375bcc68382e70
WILDLeoC:/Users/leoga/AppData/Local/Programs/Python/Python313/python.exe <leo.gaillard069gmail.com>
2025-07-23 15:59:47 +0200

    Ajout du flow Prefect dans le dossier flows

--- flows/etl_prefect.py
+++ flows/etl_prefect.py
@@ -4,43 +4,33 @@ import pandas as pd
 import time
 import os
 
-
 @task
-def fetch_stock_data(ticker: str) -> float:
+def fetch_stock_data(ticker):
     headers = {
         "x-rapidapi-key": "884dfc0466msh2ed79dd4174ccddp1e3f2ejsn7a9336a21538",
         "x-rapidapi-host": "realstonks.p.rapidapi.com"
     }
     url = f"https://realstonks.p.rapidapi.com/stocks/{ticker}/advanced"
     response = requests.get(url, headers=headers)
-    response.raise_for_status()
     return response.json().get("lastPrice", None)
-
 
 @flow(name="etl-flow")
 def etl_flow():
     tickers = ['GOOGL', 'AAPL']
-    data = {
-        'Stock_Name': [],
-        'Stock_Price': [],
-        'time': []
-    }
-
+    dict_data = {'Stock_Name': [], 'Stock_Price': [], 'time': []}
     for ticker in tickers:
         price = fetch_stock_data(ticker)
-        data['Stock_Name'].append(ticker)
-        data['Stock_Price'].append(price)
-        data['time'].append(time.strftime("%Y-%m-%d %H:%M:%S"))
+        dict_data['Stock_Name'].append(ticker)
+        dict_data['Stock_Price'].append(price)
+        dict_data['time'].append(time.time())
 
-    df = pd.DataFrame(data)
-    output_file = "financial_data.csv"
-    file_exists = os.path.exists(output_file)
-
-    df.to_csv(output_file, mode='a', header=not file_exists, index=False)
-    print("✅ Script terminé. Fichier mis à jour.")
-
+    df = pd.DataFrame.from_dict(dict_data)
+    file_exists = os.path.isfile("financial_data.csv")
+    df.to_csv("financial_data.csv", mode='a', header=not file_exists, index=False)
+    print("✅ Script terminé.")
 
 if __name__ == "__main__":
     etl_flow()
+
 
 
